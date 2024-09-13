2e6dcad431bef35cc9657e4cb980b7c455cd802d
Vlad <soboleff@mail.ru>
2024-09-13 11:40:40 +0200

    fix command

--- tg_schedule_bot/cli/bot.py
+++ tg_schedule_bot/cli/bot.py
@@ -1,15 +1,36 @@
-from aiogram import Dispatcher
+import re
+
+from aiogram import Dispatcher as BaseDispatcher
 from aiogram.filters import Command, CommandObject
 from aiogram.types import Message, BotCommand
 
 from tg_schedule_bot.cli.config import Config
 
+
+class Dispatcher(BaseDispatcher):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._commands: list[BotCommand] = []
+        self.startup.register(self._startup)
+
+    async def _startup(self, bot):
+        await bot.set_my_commands(
+            command
+            for command
+            in self._commands if re.match('^[A-Za-z0-9_]*$', command.command)
+        )
+
+    def command(self, command: BotCommand):
+        self._commands.append(command)
+        return self.message(Command(commands=command))
+
+
 dp = Dispatcher()
 
 
-@dp.message(Command(commands=BotCommand(
+@dp.command(BotCommand(
     command="пары",
-    description="[день] - показать расписание на текущий или указаный день")
+    description="[день] - показать расписание на текущий или указанный день"
 ))
 async def lectures(message: Message, command: CommandObject) -> None:
     await message.answer(Config.schedule.lectures(command.args or "сегодня"))
